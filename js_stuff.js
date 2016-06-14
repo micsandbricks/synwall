fc2a9c9e7f679cd5000aa7d25b3f942d6e558df4
micsandbricks <martin.sundequist@gmail.com>
2016-06-14 22:01:34 +0000

    add-micsandbricks

--- js_stuff.js
+++ js_stuff.js
@@ -2,6 +2,16 @@ var canvas = document.getElementById('myCanvas');
 var context = canvas.getContext('2d');
 var textelems = [];
 
-function dravTextElem() {
+var textElem = function () {
 
 }
+
+// Calls the draw function, instantiates the textelement and pushes it to the 
+// textelems vector
+var drawTextElem = function () {
+    draw();
+    var telem = textElem();
+    textelems.push(telem);
+}
+
+// Check if a textelement has been pressed and direct to a new adress
